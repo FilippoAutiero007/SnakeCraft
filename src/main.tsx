95989097d6464e95965526ad8eaf00dd8e3400be
FilippoAutiero007 <filippoautiero07@gmail.com>
2026-01-24 19:00:21 +0100

    chore: remove unused files, add env variable support, fix power-ups

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const PUBLISHABLE_KEY = "pk_test_ZmFpci1ndXBweS04Ny5jbGVyay5hY2NvdW50cy5kZXYk";
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key")
